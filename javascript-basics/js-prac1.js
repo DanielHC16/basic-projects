5c124668462d432c937ec26ed559b8daf26a3a13
Daniel Camacho <danielcamacho0416@gmail.com>
2025-07-10 19:44:55 +0800

    Minor Changes

--- javascript-basics/js-prac1.js
+++ javascript-basics/js-prac1.js
@@ -45,4 +45,33 @@ const calculator = { // Two values only. Demonstrating Object Methods.
 console.log(calculator.add(10, 10));
 console.log(calculator.multiply(15, 2));
 console.log(calculator.subtract(100, 1));
-console.log(calculator.divide(2, 4))
+console.log(calculator.divide(2, 4));
+
+// Constructor Function
+function Person(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.fullName = function() {
+        return this.firstName + " " + this.lastName; // Constructor Function Method
+    }
+}
+
+// new Person objects
+
+const johnCena = new Person ("John", "Cena", 44);
+const lebronJames = new Person ("Lebron", "James", 41);
+const derrickRamsey = new Person ("Derrick", "Ramsey", 56);
+
+lebronJames.rings = 6;
+
+console.log(lebronJames.rings);
+console.log(lebronJames.fullName());
+
+
+lebronJames.heShoots = function(){
+    action = "he SCORES";
+    return action;
+}
+
+console.log(lebronJames.heShoots());
